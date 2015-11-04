579fffa95794bad7d176b4fb437c36a6ac9e1010
inzamam malik <malikasinger@gmail.com>
2015-11-04 07:59:53 -0800

    one milestone completed

--- www/views/table/table.js
+++ www/views/table/table.js
@@ -5,5 +5,18 @@ angular.module("starter")
 
   .controller("tableController",function($scope){
 
+    $scope.tableOf = 2;
+    $scope.count = 10;
+
+    $scope.table = [];
+
+    $scope.displayTable = function(){
+
+      for(var i=1; i<$scope.count ; i++){
+        $scope.table[i] = i;
+
+      }
+
+    }
 
   });
